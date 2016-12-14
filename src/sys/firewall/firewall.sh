362fb9893b5926248e016c1468865b8a1e9cb23f
Paco Arago <farago@uji.es>
2016-12-14 15:03:19 +0000

    patched usb detection. broken links caused false duplicates
    git-svn-id: http://jira.uji.es/repos/uji/VEL/trunk@32813 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/sys/firewall/firewall.sh
+++ src/sys/firewall/firewall.sh
@@ -3,150 +3,179 @@
 
 
 setupFirewall () {
+    # Flush all chains
     iptables -F
+    iptables -t filter -F
+    iptables -t mangle -F
+    iptables -t nat -F
+    
+    # Delete unused chains
+    iptables -t filter -X
+    iptables -t mangle -X
+    iptables -t nat -X
 
-
-    #Acepta las conexiones internas por localhost
+    # Set default policies
+    iptables -P INPUT ACCEPT
+    iptables -P OUTPUT ACCEPT
+    
+    
+    
+    # Drop any potential router traffic
+    iptables -P FORWARD DROP
+    
+    
+    
+    
+    # Drop malicious 'localhost' traffic coming from interfaces
+    # different from loopback interface
+    iptables -A INPUT ! -i lo -d 127.0.0.0/8 -j DROP
+    
+    # Accept localhost traffic
     #iptables -A INPUT -d 127.0.0.1 -j ACCEPT
     iptables -A INPUT -i lo -j ACCEPT
     
     
-    #Limita los pings a 2 por segundo
+    
+    # Accept all packages belonging to connections established by this
+    # host
+    iptables -A INPUT --match state --state ESTABLISHED,RELATED -j ACCEPT
+    
+    # Load state module and accept packages that are a response to
+    # requests by this host (not invalid, nor new connections)
+    # [Removed] Using the one above, which is more restrictive
+    #iptables -A INPUT -m state ! --state  NEW,INVALID -j ACCEPT
+    
+    
+    
+    # Limit pings to 2 per second
     iptables -A INPUT -p icmp -m limit --limit 2/second -j ACCEPT
     iptables -A INPUT -p icmp -j DROP
     
     
-    #Protección contra el ataque sync flood
-    #La quitamos porque al limitar los paquetes SYN, está limitando el número de conexiones totales al equipo a 5 por segundo, lo que resulta absurdo. Además, ya se controla el volumen de paquetes por host y por tiempo. 
+    
+    # SYNC flood protection
+    # [Removed] By limiting the numbr of SYNC packages, it was limiting
+    # the number of connections to a ridiculous rate. Besides there
+    # already is a volume of packages per time limit.
     #iptables -N SYN_FLOOD
     #iptables -A SYN_FLOOD -m limit --limit 5/s --limit-burst 20 -j RETURN
     #iptables -A SYN_FLOOD -j DROP
     #iptables -A INPUT -p tcp --syn -j SYN_FLOOD
     
     
-    #Acepta todas las entradas referentes a conexiones iniciadas por esta máquina
-    iptables -A INPUT --match state --state ESTABLISHED,RELATED -j ACCEPT
     
-    #  Carga el módulo de match 'state' y acepta paquetes que sean de
-    #  respuesta a preticiones de la propia máquina (ni de nueva
-    #  conexión ni inválidos)(La ! indica negación de lo que sigue)
-    #  --> La de arriba es más restrictiva y la usamos
-    #iptables -A INPUT -m state ! --state  NEW,INVALID -j ACCEPT
-    
-    
- 
-    
-    #Drop de multicast, anycast y broadcast en general
+    #Drop multicast, anycast and broadcast in general
     iptables -A INPUT -m addrtype --dst-type BROADCAST -j DROP
     iptables -A INPUT -m addrtype --dst-type MULTICAST -j DROP
     iptables -A INPUT -m addrtype --dst-type ANYCAST   -j DROP
-
-   
-    # Drop de los paquetes dirigidos al Samba (provocaría el baneo
-    # involuntario de muchos equipos). Es posible que no lleguen aquí
-    # si son broadcast, pero por si emite algunos en unicast
+    
+    #Drop incoming udp traffic (up here, not started by this host)
+    iptables -A INPUT -p udp -j DROP
+    
+    # Drop possible unicast Samba packages (to avoid bans on the same network)
     iptables -A INPUT -p tcp -m multiport --dport 135,137,138,139 -j DROP
     iptables -A INPUT -p udp -m multiport --dport 135,137,138,139 -j DROP
-
     
     
-
-    #Lista de reglas para los nodos que están en la whitelist. Los nodos de la LCN pasarán siempre en http/https. pero drop en el resto
-    #Si un paquete llega aquí, es de la LCN. Si va a 80 o 433 pasa libremente. Si no, drop en vez de ban.
+    
+    # Whitelisted IPs. Ban not possible, no volume limits, but only
+    # http(s) connections allowed.
     iptables -N LCNACT
     iptables -A LCNACT -p tcp -m multiport --dport 80,443 -j ACCEPT
     iptables -A LCNACT -j DROP
-
-
-
-    #Creamos un set de reglas para la whitelist. 
+    
+    # Whitelist rule set (to be filled by an update script with lines
+    # as below). If source address is in the LCN set, jump to LCNACT.
     iptables -N LCN
-    # El contenido de este set lo determina un script del cron, que se descarga 
-    # periódicamente la lista de nodos de la LCN y los añade. Si un paquete viene dre la LCN, salta a LCNACT
-    #     iptables -I LCN -s "IP_ORIGEN" -j LCNACT
-
+    #iptables -I LCN -s "SOURCE_IP" -j LCNACT
     
- 
-    
-    
-   
-
-    # Es la regla que salta a la verificación de whitelist de la LCN. Si un paquete viene de la LCN, saltará a LCNACT, y si no, seguirá adelante a enfrentarse a BAN y PBAN
+    # Jump to the see if origin is in the whitelist or not.
     iptables -A INPUT -j LCN
     
     
-    #Lista de reglas de Ban: Si entra en Ban, lo elimina de pre-ban, lo añade a ban y desde entonces rechaza los paquetes.
+    #### Everything passing here might be banned ####
+    # Ban rule set (if entering BAN, remove from PREBAN, add to BAN,
+    # and drop everything).
     iptables -N BAN
-    #iptables -A BAN -j LOG --log-prefix 'Baneado: '
+    # Uncomment to debug
+    #iptables -A BAN -j LOG --log-prefix 'Banned: '
     iptables -A BAN -m recent --name prebanlist --remove
     iptables -A BAN -m recent --name banlist    --set    -j REJECT
     
-    
-    
-    # Lista de reglas de PreBan: Si entra en PreBan, reject
+    # Pre-ban rule set (If entering PREBAN, drop -better than reject,
+    # to avoid a sooner resending-).
     iptables -N PBAN
-    #iptables -A PBAN -j LOG --log-prefix 'Pre-baneado: '
-    #Cada paquete que llegue aquí, se descarta. mejor que reject, para evitar un reenvio temprano y dejar que se cumpla el timeout 
-    # Pongo rcheck para evitar que esta regla actualice la lista de last_seen de esta IP
+    # Uncomment to debug
+    #iptables -A PBAN -j LOG --log-prefix 'Pre-banned: '
+    # Using rcheck, to avoid updating the last_seen list for this IP
+    # after running this rule
     iptables -A PBAN -m recent --name prebanlist --rcheck -j DROP
     
     
     
-    #Cualquier comunicación no tcp o no dirigida a los puertos http y https provoca el baneo directo del host
+    # Any package addressed to ports different than http(s) is a cause
+    # of direct ban (no naughty peepers)
     iptables -A INPUT -p tcp -m multiport ! --dport 80,443 -j BAN
-    #iptables -A INPUT -p udp -j BAN  #El problema es que hay muchos serviicos udp inesperados que pueden banear equipos de la misma red. añadirlos? hacer drop en vez de ban? 5353(mdns),17500(dropbox),1514(ossec)
-    iptables -A INPUT -p udp -j DROP
-
-        
-    #Si supera las 20 conexiones en 2 segundos, entra en PreBan, donde se hace drop. Esto es para limitar el ratio de peticiones web
-    #Al poner update, teoricamente, si la IP no está en prebanlist, no harà match 
-    #de ningun modo. Si pusiera el set aquí, como devuelve siempre true, siempre haría el jump
-    # AVISO: cualquier valor de hitcount por encima de 20 devuelve: 'invalid argument' al ejecutarlo
+    
+    
+    
+    # If a client exceeds 20 connections per second, preban it
     #
-    iptables -A INPUT -m recent --name prebanlist --hitcount 20  --seconds 2 --update -j PBAN
+    # (using 'update', if the IP is not in prebanlist yet, it won't
+    # match; if using 'set', as it always returns true, it would
+    # always jump)
+    # WARNING: any hitcount value above 20 will return 'invalid
+    # argument' on execution
+    iptables -A INPUT -m recent --name prebanlist --hitcount 20 \
+             --seconds 2 --update -j PBAN
     
     
-    # Si está en banlist por menos de 2 horas, reject (está baneado 2 horas)
-    # Si intenta volver a acceder antes de 2 horas, al hacer un
-    # update, se resetea este contador. El rcheck evita que se
-    # actualice la banlist, por lo que a las dos horas del primer intento ya podrá conectar
-    iptables -A INPUT -m recent --name banlist --seconds $[2*3600] \
-	--rcheck -j REJECT --reject-with icmp-host-prohibited
+    # Clients are banned for two hours
+    #
+    # If in banlist for less than 2h, reject. If trying to access
+    # again before 2h, the counter would reset after the
+    # update. 'rcheck' avoids this, so after 2h it will be un-banned
+    iptables -A INPUT -m recent --name banlist \
+             --seconds $[2*3600]	--rcheck \
+             -j REJECT --reject-with icmp-host-prohibited
     
     
-   
     
-    #Si Después de pasar todas las barreras, es una conexión a http o https, la deja pasar, y actualiza la lista de preban
-    #iptables -A INPUT -p tcp -m multiport --dport http,https -m recent --name prebanlist --set -j ACCEPT
-    #iptables -A INPUT -m recent --name prebanlist --set -j ACCEPT
-    iptables -A INPUT -p tcp  --dport http -m recent --name prebanlist --set -j ACCEPT
-    iptables -A INPUT -p tcp  --dport https -m recent --name prebanlist --set -j ACCEPT
-    #[ "$1" == "ssl" ] && iptables -A INPUT -p tcp  --dport https -m recent --name prebanlist --set -j ACCEPT
+    # Allow http and https connections from any source and update
+    # preban list
+    iptables -A INPUT -p tcp  --dport http -m recent \
+             --name prebanlist --set -j ACCEPT
+    iptables -A INPUT -p tcp  --dport https -m recent \
+             --name prebanlist --set -j ACCEPT
+    #iptables -A INPUT -p tcp -m multiport --dport http,https |
+    #         -m recent --name prebanlist --set -j ACCEPT
     
-    #Si Después de pasar todas las barreras, va a otro puerto, reject
+    
+    
+    # If anything leaked up to here, simply reject (with a host
+    # prohibited response, which is quite unexpected)
     iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
     
     
-    # Ver las listas de recent: /proc/net/ipt_recent/ o en /proc/net/xt_recent/
-
     
-    #Quitar el baneo
-    #echo -150.128.49.XX >/proc/net/xt_recent/banlist
-
-
-    #Ver los logs
-    #tail -f /var/log/messages | grep --color=auto 49.XX &
+    
+    
+    
+    
+    ##### TOOLS #####
+    
+    # See recent list:
+    #cat /proc/net/ipt_recent/
+    #cat /proc/net/xt_recent/
+    
+    
+    # Remove ban:
+    #echo -8.8.8.8 >/proc/net/xt_recent/banlist
+    
+    
+    # See logs:
+    #tail -f /var/log/messages | grep --color=auto 8.8.8.8 &
     #tail -f /var/log/apache2/access.log &
-
-
-    #Acepta conexiones nuevas al puerto https
-    #[ "$1" == "ssl" ] && iptables -A INPUT -p TCP --dport 443 -j ACCEPT  
-
-    #Acepta conexiones nuevas al puerto http
-    #iptables -A INPUT -p TCP --dport 80  -j ACCEPT
-
-    #Rechaza cualquier otro paquete (incluso pings)   
-    #iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
-
-}    
-
+    
+    return 0
+}
